4a9b861f63b293d07759c7af500a041b4e0f7432
dwq9172 <39833979+dwq9172@users.noreply.github.com>
2018-06-01 17:03:24 -0400

    Update HashTables.py

--- HashTables.py
+++ HashTables.py
@@ -4,37 +4,42 @@ import time
 
 load_factors = [0.5, 0.75]
 loadFactor = load_factors[0]
-total_time = 0
-search_count = 0
+ttl_time = 0
+search_cnt = 0
 
-def search_time(isbn, hash_table):
-    global total_time
-    global search_count
+
+def measure_search_time(isbn, hashtbl):
+    global ttl_time
+    global search_cnt
     start = time.clock()
     print('Searching ISBN {} in library'.format(isbn))
-    if isbn in hash_table:
-        print(hash_table.get(isbn))
-        found = True
+    if isbn in hashtbl:
+        print(hashtbl.get(isbn))
+        found=True
     else:
         print("Book with this ISBN number is not found in the library.")
-        found = False
+        found=False
     stop = time.clock()
     elpSeconds = stop - start
-    total_time += elpSeconds
-    search_count += 1
-    print('Search took {:5f} seconds, found: {}, average: {:5f}'.format(elpSeconds, found, total_time / search_count))
-    return elpSeconds
-
+    ttl_time += elpSeconds
+    search_cnt += 1 
+    print("Search took {:5f} seconds, found: {}, average: {:5f}".format(elpSeconds, found, ttl_time/search_cnt))
+    return elpSeconds 
+    
 def main():
     """ This program hashes Books.txt.
         1. Change the hash function as indicated below.
         2. Change the rehash function as indicated below.
-        3. How many times does this hash table make itself bigger (change its size)?
+        3. How many times does this hash table make itself bigger (change its size)? 9
         4. Which load factor in the range .5 to .9 in steps of .25 is optimal in the sense
-        of least time to build the hash table given a start size of 11 on Books.txt?
-        5. Which hash works best?
+        of least time to build the hash table given a start size of 11 on Books.txt?  ->0.5
+            0.5   - 0.2 second build time
+            0.75  - 0.5 second build time
+        5. Which hash works best? 
         The original with a load factor of .5 using  the first character key and linear probing
         or your modified/optimized with steps 1-4 hash?
+            original, build:  search:
+            modified, build: 0.2 (0.5 load factor) search average: 0.000259
         6. Write a function(s) that run after main is done to prove your conclusions for steps 5 & 6.
     """
     start = time.clock()
@@ -47,30 +52,32 @@ def main():
                 library.put(int(d[2]), Book(d[0], d[1], d[2]))
                 d = []
     stop = time.clock()
-    print('Building hash took {:5f} seconds with load factor of {}'.format(stop-start, loadFactor))
-    print('Times required to resize:{:5d}\n'.format(library.resizes))
-
-    #isbn = 9780821336953
-    #print('You entered ISBN {}'.format(isbn))
-    #if isbn in library:
-    #    print(library.get(isbn))
-    #else:
-    #    print("I couldn't find a book with that ISBN number.'")
+    print("Building hash took {:5f} seconds with load factor of {}".format(stop-start, loadFactor))
+    print("Times required to resize {:5d}".format(library.resizes))
+    
+                
+    """
+    isbn = 9780821336953
+    print('You entered ISBN {}'.format(isbn))
+    if isbn in library:
+        print(library.get(isbn))
+    else:
+        print("I couldn't find a book with that ISBN number.'")
     #isbn = int(input('enter ISBN: '))
-    #print('You entered ISBN {}'.format(isbn))
-    #if isbn in library:
-    #    print(library.get(isbn))
-    #else:
-    #    print("I couldn't find a book with that ISBN number.'")
-    #print('Return value for library.get({}) is "{}"'.format(isbn, library.get(isbn)))
-
-    isbns = [9780821336953, 9781464802027, 9781464802126, 9780821350164, 9781464899999]
+    isbn = 9781464802027 
+    print('You entered ISBN {}'.format(isbn))
+    if isbn in library:
+        print(library.get(isbn))
+    else:
+        print("I couldn't find a book with that ISBN number.'")
+    print('Return value for library.get({}) is "{}"'.format(isbn,library.get(isbn)))
+    """
+    
+    
+    isbns = [9780821336953, 9781464802027, 9781464802126, 9780821350164, 9781464899999] 
     for isbn in isbns:
-        search_time(isbn, library)
-        print('\t')
-
-    print('ANSWERS\nQuestion 3: 0 times\nQuestion 4: load factor of 0.5\nQuestion 5: Modified Hash')
-
+        measure_search_time(isbn, library)
+    
 class Book:
     def __init__(self, n, a, i):
         self.name = n
@@ -79,6 +86,7 @@ class Book:
 
     def __str__(self):
         return self.name + '\n' + self.author + '\n' + self.isbn
+
     
 class HashTable:
     def __init__(self):
@@ -93,7 +101,8 @@ class HashTable:
     
     def put(self,key,data):
         # make the hash table bigger
-        if self.count / self.size > loadFactor:
+        if self.count / self.size > loadFactor: 
+            self.resizes += 1
             nextPrime = findNextPrime(2*self.size + 1)
             oldslots = self.slots[:]
             olddata = self.data[:]
@@ -105,6 +114,8 @@ class HashTable:
             for index in range(len(oldslots)):
                 if oldslots[index] != None:
                     self.put(oldslots[index], olddata[index])
+
+
         hashvalue = self.hashfunction(key,len(self.slots))
 
         if self.slots[hashvalue] == None:
@@ -115,10 +126,15 @@ class HashTable:
             if self.slots[hashvalue] == key:
                 self.data[hashvalue] = data  #replace
             else:
+                #rehash_cnt = 0
+                #nextslot = self.rehash(hashvalue,len(self.slots),rehash_cnt)
                 nextslot = self.rehash(hashvalue,len(self.slots))
+                #rehash_cnt += 1
                 while self.slots[nextslot] != None and \
                                 self.slots[nextslot] != key:
+                    #nextslot = self.rehash(nextslot,len(self.slots), rehash_cnt)
                     nextslot = self.rehash(nextslot,len(self.slots))
+                    #rehash_cnt += 1
 
                 if self.slots[nextslot] == None:
                     self.slots[nextslot]=key
@@ -126,25 +142,42 @@ class HashTable:
                     self.count += 1
                 else:
                     self.data[nextslot] = data #replace
+
+
 ################################################################################
     ''' Rewrite this hash function to fold the key by making groups of 4 (the first 4 digits,
         the second 4 digits, and the third 4 digits, adding them together, and then dividing
         by the length of the hash table.
     '''
     def hashfunction(self,key,size):
-        ''' Change this!!!! 
+        '''  
         '''
         s=str(key)
-        a, b, c = int(s[:4]), int(s[4:8]), int(s[8:12])
-        return (a + b + c)%size
+        #print("hashfunction: key {}, size {}, hash {}".format(key, size, (int(s[0]))%size))
+        return (int(s[0]))%size
+        """
+        s1 = int(s[0:4])
+        s2 = int(s[4:8])
+        s3 = int(s[8:12])
+        s = s1 + s2 + s3
+        hash = (s1+s2+s3)%size
+        #print("hashfunction: size {}, hash {}".format(size, hash))
+        return hash 
+        """
 ################################################################################
+
 
 ################################################################################
     ''' Rewrite this rehash function to use quadratic probing.
     '''
-    def rehash(self,oldhash,size, rehCount=0):
-        count = list(range(1, 3 * size + 1, 2))
-        return (oldhash+count[rehCount])%size
+    def rehash(self,oldhash,size,rehash_cnt=0):
+        return (oldhash+1)%size
+        """
+        #increments the hash value by 1, 3, 5, 7, 9, and so on
+        increments = list(range(1,3*size+1,2)) 
+        #print("rehash, cnt {}, size {}, list {}".format(rehash_cnt, size, increments))
+        return (oldhash+increments[rehash_cnt])%size
+        """
 ################################################################################
 
     def get(self,key):
@@ -174,6 +207,8 @@ class HashTable:
     def __len__(self):
         return self.count
 
+
+
 def isPrime(n):
     ''' Returns True if n is a prime number.  False, otherwise.
     '''
@@ -187,6 +222,7 @@ def isPrime(n):
             return False
     return True
 
+
 def findNextPrime(n):
     ''' Finds the next prime number >= n.
     '''
@@ -195,6 +231,7 @@ def findNextPrime(n):
     while not isPrime(n):
         n += 2
     return n
+
 
 if __name__ == '__main__':
     main()
